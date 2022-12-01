09204ff71a59189e69198c00182b29e08370b15e
Lucas Eduardo Hoeltgebaum <lucas.hoeltgebaum@workdb.com.br>
2022-12-01 13:49:39 -0300

    default value for config paths

--- API/App/src/Configs.py
+++ API/App/src/Configs.py
@@ -31,20 +31,20 @@ class Configs:
 		self.youtubeDL_options = YoutubeDLOptions()
 		self.youtubeDL_options.prepare_mp3_download()
 
-	def set_cache_dir(self, path: str):
+	def set_cache_dir(self, path: str = None):
 		self.cache_dir = path or os.path.join('/', 'var', 'log', 'youtube_downloader') if is_unix() else os.path.join(get_app_path(), 'cache')
 		logger.info(f'Cache dir is {self.cache_dir}')
 		if not os.path.exists(self.cache_dir):
 			logger.info(f'Cache dir not created, creating.')
 			os.mkdir(self.cache_dir)
 
-	def set_mp3_output_path(self, path: str):
+	def set_mp3_output_path(self, path: str = None):
 		self.mp3_output_path = path or os.path.join('/', 'var', 'lib', 'youtube_downloader') if is_unix() else os.path.join(get_app_path(), 'Downloads')
 		if not os.path.exists(self.mp3_output_path):
 			logger.info(f'Output path not created, creating')
 			os.mkdir(self.mp3_output_path)
 
-	def set_mp4_output_path(self, path: str):
+	def set_mp4_output_path(self, path: str = None):
 		self.mp4_output_path = path or os.path.join('/', 'var', 'lib', 'youtube_downloader') if is_unix() else os.path.join(get_app_path(), 'Downloads')
 		if not os.path.exists(self.mp4_output_path):
 			logger.info(f'Output path not created, creating')
